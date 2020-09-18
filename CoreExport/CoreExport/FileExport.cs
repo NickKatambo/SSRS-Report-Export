e9bbe2205bbe70da6f37aaddaa6490f844c54628
nick <yannick.katambo@outlook.com>
2020-09-19 00:49:24 +0200

    Initial commit

---
+++ CoreExport/CoreExport/FileExport.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoreExport
+{
+    public class FileExport : IFileExport
+    {
+        public string ExportAsPdf(int id, string url, string exportPath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CoreExport/CoreExport/IFileExport.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreExport
+{
+    public interface IFileExport
+    {
+        string ExportAsPdf(int id, string url, string exportPath);
+    }
+}
